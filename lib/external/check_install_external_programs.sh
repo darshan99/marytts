a9aca3e1004a634458289d37ad14f8b52c6096f7
Darshan <mailshahdarshan@gmail.com>
2018-02-14 00:32:30 +0530

    Initial

--- lib/external/check_install_external_programs.sh
+++ lib/external/check_install_external_programs.sh
@@ -1,44 +1,4 @@
-#!/bin/bash
-# Copyright 2011 DFKI GmbH.
-# All Rights Reserved.  Use is subject to license terms.
-#
-# This file is part of MARY TTS.
-#
-# MARY TTS is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# HMMVoice creation for MARY 4.0 - Checking and installing external programs
-# more information in: http://mary.opendfki.de/wiki/HMMVoiceCreationMary4.0
-# 
-# Created by Marcela Charfuelan (DFKI) Mon May 16 15:05:07 CEST 2011
-# use:
-#   ./check_install_external_programs.sh [-check|-install][additional paths]
-
-
-# Exit on all errors
-set -e
-# Trap exit
-trap trap_exit_handler EXIT
-
-
-# Handle exit trap
-function trap_exit_handler() {
-    # Backup exit status if you're interested...
-    local exit_status=$?
-    # Change value of $?
-    true
-    #echo $?
-    #echo trap_handler $exit_status
-} # trap_exit_handler()
+#!/bin/sh
 
 
 if [ $# -lt 1 ] ; then
@@ -254,7 +214,7 @@ if which HHEd > /dev/null; then
     # CM directory         - Convert models to pdf for speech synthesizer
     htsCommands=`$hhedProg -Q | grep CM | awk '{print $1}'`
     # here it is used quotes for htsCommand because it can be empty ""
-    if [ "$htsCommands" == "CM" ]; then
+    if ["$htsCommands" == "CM"]; then
        htkPath=`dirname $hhedProg`      
        echo "HTK HHED contains HTS commands like CM "
        echo "HTK ok"
